e6bd9012145046ae4b7603d30cccafc9030839a8
Seneth <126297209+sen3th@users.noreply.github.com>
2026-04-11 14:16:38 -0400

    added tab switching config to js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const folderButton = document.getElementById("folderButton");
 const caseBoard = document.getElementById("caseBoard");
+const tabs = document.querySelectorAll(".tab");
+const papers = document.querySelectorAll(".paper");
+
 
 folderButton.addEventListener("click", function(){
     caseBoard.classList.toggle("hidden");
+})
+
+tabs.forEach(function(tab){
+    tab.addEventListener("click", function(){
+        const selectedPanel = tab.dataset.panel;
+
+        tabs.forEach(function (item){
+            item.classList.remove("active");
+        });
+        tab.classList.add("active");
+        papers.forEach(function (paper){
+            if (paper.dataset.panel === selectedPanel){
+                paper.classList.add("show");
+            }else{
+                paper.classList.remove("show");
+            }
+        })
+    })
 })
